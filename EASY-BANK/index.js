f42894ac3254d54833de6d58c54fe959689cdeb5
Sherwin Robert <89347394+SherwinRobert@users.noreply.github.com>
2021-12-19 13:40:38 +0530

    Delete EASY-BANK directory

--- EASY-BANK/index.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".menu-button").addEventListener("click",clicker)
-
-function clicker(){
-    console.log("button is pressed")
-    document.querySelector(".floating-menu").classList.toggle("translator")
-}
